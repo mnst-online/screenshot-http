0635395c8c850cc28ead33eec01372bc86f087c2
Sirawat <49577823+trtor@users.noreply.github.com>
2024-01-29 22:11:03 +0700

    Add files via upload

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,31 @@
 import express from "express";
 import screenshot from "screenshot-desktop";
+import { join } from "path";
+import NodeCache from "node-cache";
+
+const pngCache = new NodeCache({ stdTTL: 0.5, checkperiod: 120 });
+const pngCacheKey = "png";
 
 const app = express();
 const port = 3000;
 
-app.get("/", async (req, res) => {
+app.use(express.static(join(__dirname, "public")));
+
+app.get("/", (_, res) => {
+  res.sendFile(join(__dirname, "public", "index.html"));
+});
+
+app.get("/png", async (_, res) => {
   try {
+    const cacheData = pngCache.get(pngCacheKey);
+    if (cacheData) {
+      res.contentType("image/png");
+      return res.send(cacheData);
+    }
     const img = await screenshot();
     res.contentType("image/png");
     res.send(img);
+    pngCache.set(pngCacheKey, img);
   } catch (error) {
     console.error("Error capturing screenshot:", error);
     res.status(500).send("Internal Server Error");
